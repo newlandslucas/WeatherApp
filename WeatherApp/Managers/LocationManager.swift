dbcd616f96314d4ddcb4f7d14607ef106117aafa
newlandslucas <lucasnewlands.m@gmail.com>
2022-04-18 16:29:09 -0300

    feat: WelcomeView

--- WeatherApp/Managers/LocationManager.swift
+++ WeatherApp/Managers/LocationManager.swift
@@ -14,4 +14,23 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     @Published var location: CLLocationCoordinate2D?
     @Published var isLoading = false
     
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation() {
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+        isLoading = false
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error getting location", error)
+        isLoading = false
+    }
 }
---
+++ WeatherApp/Views/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Lucas Newlands on 18/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    
+    var body: some View {
+        VStack {
+            
+            if let location = locationManager.location {
+                Text("Your coordinates are: \(location.longitude), \(location.latitude)")
+            } else {
+                if locationManager.isLoading {
+                    ProgressView()
+                } else {
+                    WelcomeView()
+                        .environmentObject(locationManager)
+                }
+            }
+          
+        }
+        .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+        .preferredColorScheme(.dark)
+    }
+       
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
